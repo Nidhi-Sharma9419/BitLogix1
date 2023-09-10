11ea8cfc3665db2f1daea6845356c4e806b4d61b
Nidhi-Sharma9419 <114356358+Nidhi-Sharma9419@users.noreply.github.com>
2023-09-10 10:22:24 +0530

    ConnectWallet and SelectRole

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import './App.css';
+
+
+
+
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Make sure to import Switch
+
+
+
+import ConnectWallet from './components/ConnectWallet';
+import SelectRole from './components/SelectRole';
 
 function App() {
   return (
-    <>
-    </>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<ConnectWallet/>} />
+        <Route path="/SelectRole" element={<SelectRole/>}/>
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
+
