795fdf655e0042fa2134dfb9a27d04af41a659c7
Nidhi-Sharma9419 <114356358+Nidhi-Sharma9419@users.noreply.github.com>
2023-09-17 00:25:08 +0530

    Updations

---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import SelectRole from './SelectRole';
+
+
+export default function Home() {
+    return (
+        <>
+           <SelectRole/> 
+</>
+
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+//import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+
+import ReactDOM from 'react-dom';
+import { Web3ReactProvider } from '@web3-react/core';
+import { Web3Provider } from "@ethersproject/providers";
+
+
+
+function getLibrary(provider) {
+  return new Web3Provider(provider);
+}
+
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <App />
+    </Web3ReactProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
