41ad72c7983704b3ec3307707e58ea64ffa4319b
The-GO0d-Guy <malaha814@gmail.com>
2023-09-20 01:14:13 +0530

    update folder structure and navbar

--- src/App.js
+++ src/App.js
@@ -5,12 +5,12 @@ import { InjectedConnector } from '@web3-react/injected-connector';
 import { useWeb3React } from '@web3-react/core';
 import { Box, Typography } from '@mui/material';
 import { Button } from '@mui/base';
-import Home from './Home';
+import Home from './components/Home';
 import AboutUs from './AboutUs';
 import Recipient from './components/Recipient';
-import SelectRole from './components/SelectRole';
+// import SelectRole from './components/SelectRole';
 import Delivery from './components/Delivery';
-import Entity from './components/Entity';
+import Enterprise from './components/Enterprise';
 import ProductDetails from './components/ProductDetails';
 import AddDelivery from './components/AddDelivery';
 import "react-datepicker/dist/react-datepicker.css";
@@ -25,43 +25,8 @@ function App() {
   const[address, setAddress] = useState("");
   const{activate, active, account} = useWeb3React();
 
-  useEffect(()=>{
-    if(account){
-      console.log("Account", account);
-      setAddress(account);
-      
-
-    }
-  }, [account]);
-
-  const connectWallet = async() => {
-    console.log("Connecing to wallet...");
-    try{
-      await activate(injectedConnector);
-      console.log("wallet connected");
-      console.log("Account",account);
-      setConnected(true);
-    
-    }catch(error){
-      console.error("Failed to connect", error);
-    }
-  };
-
   return (
     <Router>
-      <nav style={{ margin: 10 }}>
-          <Link to="/Home" style={{ padding: 5 }}>
-          Home
-          </Link>
-          <Link to="/aboutus" style={{ padding: 5 }}>
-          About
-          </Link>
-          <button
-          onClick={connectWallet}>
-            Connect Wallet
-          </button>
-          <p>{address}</p>
-      </nav>
 
       <Routes>
         <Route path="/Home" element={<Home />}></Route>
@@ -69,7 +34,7 @@ function App() {
         
         <Route path="/recipient" element={<Recipient />} />
         <Route path='/delivery' element={<Delivery />} />
-        <Route path="/enterprise" element={<Entity />} />
+        <Route path="/enterprise" element={<Enterprise />} />
         <Route path="/addproduct" element={<ProductDetails />} />
         <Route path="/adddelivery" element={<AddDelivery />} />
       </Routes>
--- src/components/SelectRole.js
+++ src/components/SelectRole.js
@@ -1,39 +1,68 @@
-import { useState } from 'react';
-export default function SelectRole(){
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import Enterprise from "./Enterprise";
+import Recipient from "./Recipient";
+import Navbar from "./Navbar";
 
-   
+// import { Link, useNavigate } from "react-router-dom";
+export default function Register() {
+  //   const navigate = useNavigate();
 
-  async function entity(){
-   
-  }
-
-  async function recipient(){
-    
-  }
-
-
-    return (
-        <div className="App">
-          <div className='App-Header'>
-            <div className='description'>
-            <h1>BitLogix</h1>
-            <h2>Select Role</h2>
-            </div>
-            
-            <div className='custom-buttons'>
-            <button
-            onClick={entity}
-            style={{backgroundColor: "blue"}}>Entity</button>
-          <button
-          onClick={recipient}
-          style={{backgroundColor: "pink"}}>Recipient</button>
-            </div>
-          
-          
-          </div>
-          
+  const [type, setType] = useState("");
+  return (
+    <>
+  <Navbar/>
+    <div className="flex justify-center items-center">
+      <div className="w-1/2 mt-12  -me-40 hidden lg:block">
+        <img src="../reg.png" />
+      </div>
+      <div className="flex flex-col  justify-center items-center h-[11/12] mt-32 lg:mt-0 " >
+        <div>
+          <p className="font-bold text-4xl">select your role</p>
         </div>
-      );
-    }
-    
-
+        <div className="flex justify-between gap-5 mt-5">
+          <button
+            className={`${
+              type == "Enterprise"
+                ? "bg-green-400 text-gr font-bold outline outline-green-600"
+                : "outline outline-offset-2 outline-green-500 outline-1 hover:outline-2 hover:bg-green-200"
+            } rounded-full p-3 w-32 `}
+            onClick={() => setType("Enterprise")}
+          >
+            Enterprise
+          </button>
+          <button
+            className={`${
+              type == "Recipient"
+                ? "bg-green-400 text-gr font-bold outline outline-green-600"
+                : "outline outline-offset-2 outline-green-500 outline-1 hover:outline-2 hover:bg-green-200"
+            } rounded-full p-3 w-32 `}
+            onClick={() => setType("Recipient")}
+          >
+            Recipient
+          </button>
+        </div>
+        <div>
+          {type ? (
+            <>
+              {type == "Enterprise" ? (
+                <>
+                 <Link to="/enterprise"></Link>
+                </>
+              ) : (
+                <>
+                  <div>
+                  <Link to="/recipient"></Link>
+                  </div>
+                </>
+              )}
+            </>
+          ) : (
+            <></>
+          )}
+        </div>
+      </div>
+    </div>
+    </>
+  );
+}
