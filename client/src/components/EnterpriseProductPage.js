d573ab5116d0e273eee37b7f903345aff50e0990
suraj pro <71000909+suraj719@users.noreply.github.com>
2023-09-22 23:38:40 +0530

    feat: enhance ui

--- client/src/components/EnterpriseProductPage.js
+++ client/src/components/EnterpriseProductPage.js
@@ -23,27 +23,55 @@ export default function EnterpriseProductPage() {
     <>
       <div className="flex flex-wrap items-center justify-end px-5 py-5">
         <Link to="/addproduct">
-        <button className=" bg-green-400 p-2 px-3 rounded-full">
-          Add product
-        </button>
+          <button className=" bg-green-400 p-2 px-3 rounded-full">
+            Add product
+          </button>
         </Link>
       </div>
-      <div>
+      <div className="flex flex-wrap">
         {isloading ? (
           <>loading</>
         ) : (
           <>
             {data ? (
               <>
-                <ol className="list-decimal">
+                <div className="flex flex-col gap-5 p-4">
                   {data.map((pro, index) => {
                     return (
-                      <li key={index}>
-                        <p className="font-bold text-2xl">{pro.name}</p>
-                      </li>
+                      <div
+                        key={index}
+                        className="bg-gray-100 hover:bg-gray-200 rounded-lg px-5 py-3 md:w-[96vw] w-[90vw]"
+                      >
+                        <div className="flex justify-between">
+                          <Link to={`/product/${pro._id}`}>
+                            <p className="font-bold text-2xl hover:underline">
+                              {pro.name}
+                            </p>
+                          </Link>
+                          <div>
+                            {pro.delivered ? (
+                              <p className="font-bold text-gray-500">
+                                Delivered
+                              </p>
+                            ) : (
+                              <>
+                                <div className="mt-1 flex items-center gap-x-1.5">
+                                  <div className="flex-none rounded-full bg-emerald-500/20 p-1">
+                                    <div className="h-1.5 w-1.5 rounded-full bg-emerald-500"></div>
+                                  </div>
+                                  <p className=" font-bold leading-5 text-gray-500">
+                                    In-transit
+                                  </p>
+                                </div>
+                              </>
+                            )}
+                          </div>
+                        </div>
+                        <p>To: {pro.recipientaddress}</p>
+                      </div>
                     );
                   })}
-                </ol>
+                </div>
               </>
             ) : (
               <>
